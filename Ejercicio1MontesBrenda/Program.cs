9073a6468e44ba449f92f005fba72b6c9a3bbdd9
brendamontes2510@hotmail.com <montesb140@hotmail.com>
2022-06-25 19:07:55 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio1MontesBrenda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio1MontesBrenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar;
+            double euro;
+            double pesos;
+            double division;
+
+            Console.Write("Ingrese la cantidad de pesos: ");
+            pesos = double.Parse(Console.ReadLine());
+
+            dolar = pesos / 230;
+            euro = dolar / 1.17;
+
+            Console.WriteLine($"Los dolares que puede comprar son: {dolar} ");
+            Console.WriteLine($"Los euros que puede comprar son: {euro}");
+
+            Console.ReadLine();
+        }
+    }
+}
